a72bce7e23c2826590e36297ef8405414be5ee19
beatalochowska <beata.lochowska@gmail.com>
2020-03-22 10:34:20 +0100

    Updates

---
+++ precache-manifest.f2155dd6f8bc81a1627e28d7a672a4df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e36a795649c6d401b67f7fcba01c0a23",
+    "url": "/to_do_app_redux/index.html"
+  },
+  {
+    "revision": "aabcb58dbc78715c55e6",
+    "url": "/to_do_app_redux/static/js/2.2b0939de.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/to_do_app_redux/static/js/2.2b0939de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea765f6edf2be984ed44",
+    "url": "/to_do_app_redux/static/js/main.66fa27c4.chunk.js"
+  },
+  {
+    "revision": "65602754ac8828b7e4f3",
+    "url": "/to_do_app_redux/static/js/runtime-main.a1a271f9.js"
+  }
+]);
